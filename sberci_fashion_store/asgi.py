38d201c25aa6bf3f7054892421ea4cf58407db4d
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2022-06-16 13:44:05 -0300

    first commit

---
+++ sberci_fashion_store/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sberci_fashion_store project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sberci_fashion_store.settings')
+
+application = get_asgi_application()
