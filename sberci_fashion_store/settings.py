f06a7d3e33ae7ad4565e8fd29515f4b2869d02a3
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2022-06-16 14:29:39 -0300

    first create simple page

--- sberci_fashion_store/settings.py
+++ sberci_fashion_store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard_store.apps.Dashboard_storeConfig',
 ]
 
 MIDDLEWARE = [
